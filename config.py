81789c11f58e74d1fc95c951aece47ffeda894f2
AmirGadami <aghad051@uottawa.ca>
2025-04-01 11:05:56 -0400

    update config file

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+import io
+from dotenv import load_dotenv
+
+
+load_dotenv()
+os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
+os.environ['ANTHROPIC_API_KEY'] = os.getenv('ANTHROPIC_API_KEY')
+
+
+OPENAI_MODEL = "gpt-4o"
+CLAUDE_MODEL = "claude-3-5-sonnet-20240620"
+
+
+system_message = "You are an assistant that reimplements Python code in high performance C++ for an M1 Mac. "
+system_message += "Respond only with C++ code; use comments sparingly and do not provide any explanation other than occasional comments. "
+system_message += "The C++ response needs to produce an identical output in the fastest possible time. Keep implementations of random number generators identical so that results match exactly."
