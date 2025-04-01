45b368840a19c9eeb98b637395110bab1d8368c7
AmirGadami <aghad051@uottawa.ca>
2025-04-01 16:18:24 -0400

    add the streamming functions for llms

--- llms.py
+++ llms.py
@@ -22,5 +22,48 @@ def optimized_gpt(python):
         print(fragment, end="",flush=True)
     write_output(reply)
 
+def optimized_claude(python):
+    result = claude.messages.stream(
+        model = CLAUDE_MODEL,
+        max_tokens=200,
+        system=system_message,
+        messages=[{'role':'user','content':user_prompt_for(python)}]
+    )
+    reply = ""
+    with result as stream:
+        for text in stream.text_stream:
+            reply += text
+            print(text,end='', flush=True)
+    write_output(reply)
 
 
+def stream_gpt(python):
+    stream = openai.chat.completions.create(model=OPENAI_MODEL, messages=messages_for(python), stream=True)
+    reply = ""
+
+    for chunk in stream:
+        fragments = chunk.choices[0].delta.content or ""
+        reply += fragments
+        yield reply.replace('```cpp\n','').replace('```','')
+
+def stream_claude(python):
+    result = claude.messages.stream(
+        model=CLAUDE_MODEL,
+        max_tokens=2000,
+        system=system_message,
+        messages=[{"role": "user", "content": user_prompt_for(python)}],
+    )
+    reply = ""
+    with result as stream:
+        for text in stream.text_stream:
+            reply += text
+            yield reply.replace('```cpp\n','').replace('```','')
+
+
+
+if __name__ =="__main__":
+    from config import pi
+
+    # exec(pi)
+    optimized_claude(pi)
+    stream_claude(pi)
